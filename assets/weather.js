dbf3cf20c95d5090525c7a8ec8995ed828bb9d88
orangeblack13 <AndrewJKleiner@gmail.com>
2020-06-15 21:29:59 -0400

    2 of three render functions done, talking to API

--- assets/weather.js
+++ assets/weather.js
@@ -7,28 +7,65 @@ var citiesList = document.getElementById("citiesList")
 var cities = [];
 var APIKey = "1185a689f92365d49ec83f25a53f8191";
 cityName = "";
-var mainQueryURL = "api.openweathermap.org/data/2.5/forecast/daily?q="+cityName+"&cnt={cnt}&appid="+APIKey;
+mainBodyObj = {
+    cityName : " ",
+    date : "",
+    weatherIcon : "",
+    temp: "",
+    windSpd: "",
+    UV: "",
+}
+var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Philadelphia&appid=" + APIKey
 
 function renderCities(){
     for (i = cities.length; i < cities.length; i--){
-        var cityName = $("<li>");
-        cityName.value = cities[i];
-        citiesList.append(cityName);
+        var cityListName = $("<li>");
+        cityListName.value = cities[i];
+        citiesList.append(cityListName);
     }
 }
 
+function kelvToFar(temp){
+    return(temp - 273.15) * 1.80+32;
+}
 
+function renderMain(){
+    cityName = passText.value;
+    var textHead = $("<h1>")
+    cityName = current.name;
+    textHead.value = cityName + " " + moment().subtract(10, 'days').calendar() + " " + weatherIcon;
 
+    var tempPar = $("<p>");
+    currentTemp = kelvToFar(current.main.temp).value;
+    tempPar.value = currentTemp;
 
+    var humPar = $("<p>");
+    currentHum = current.main.humidity;
+    humPar.value = currentHum;
 
-searchBtn.addEventListener("click", function(event){
-    $.ajax({
-        url: mainQueryURL,
-        method: "GET"
-    }).then(function(populate){
-        cityName = passText.value
-        cities.push(cityName);
-        console.log(cities);
-        console.log(cityName);
-    })
+    var windPar = $("<p>");
+    currentWind = current.wind.speed
+    windPar.value = currentWind + " MPH"
+}
+
+$.ajax({
+    url: currentQueryURL,
+    method: "GET"
+}).then(function(current){
+   $(searchBtn).on("click", function(){
+    cities.push(passText.value)
+    cityName
+}) 
 })
+
+
+
+// searchBtn.addEventListener("click", function(event){
+
+//     }).then(function(populate){
+//         cityName = passText.value
+//         cities.push(cityName);
+//         console.log(cities);
+//         console.log(cityName);
+//     })
+// })
