97738fa7b90723612938f8ab341b58b16ea1de19
orangeblack13 <AndrewJKleiner@gmail.com>
2020-06-17 21:24:01 -0400

    Local Storage working in full, site now holds previous searches when opened

--- assets/weather.js
+++ assets/weather.js
@@ -4,7 +4,9 @@ var mainTxt = document.getElementById("mainBody");
 var daily = document.getElementById("daily");
 var citiesSec = document.getElementById("cities")
 var citiesList = document.getElementById("citiesList");
-var cities = [];
+var savedCities = localStorage.getItem("cities");
+
+var cities = (!savedCities) ? [] : JSON.parse(savedCities);
 var APIKey = "1185a689f92365d49ec83f25a53f8191";
 var lon;
 var lat;
@@ -15,9 +17,9 @@ var dayThree = document.getElementById("dayThree");
 var dayFour = document.getElementById("dayFour");
 var dayFive = document.getElementById("dayFive");
 mainBodyObj = {
-    cityName : " ",
-    date : "",
-    weatherIcon : "",
+    cityName: " ",
+    date: "",
+    weatherIcon: "",
     temp: "",
     windSpd: "",
     UV: "",
@@ -25,56 +27,85 @@ mainBodyObj = {
 citiesObj = {
     current: "",
     second: "",
-    third:"",
+    third: "",
     fourth: "",
     fifth: "",
 }
 
+
+
 const makeUrl = (city) => {
     var currentQueryURL =
-      'https://api.openweathermap.org/data/2.5/weather?q=' +
-      city +
-      '&appid=' +
-      APIKey;
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        city +
+        '&appid=' +
+        APIKey;
     return currentQueryURL;
-  };
+};
 
-  const makeUVUrl = (lon, lat) => {
-      var UVQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`
+
+const makeUVUrl = (lon, lat) => {
+    var UVQueryURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${lon}`
     return UVQueryURL;
-  }
+}
 
-  const fiveDayUrl = () => {
-      var fiveDayQueryUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`
-      return fiveDayQueryUrl;
-  } 
+const fiveDayUrl = () => {
+    var fiveDayQueryUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKey}`
+    return fiveDayQueryUrl;
+}
 
-  const renderFiveDay = (fiveDay) => {
+const renderFiveDay = (fiveDay) => {
     $("#fiveRow").empty();
-    fiveDay.forEach(day =>{
+    fiveDay.forEach(day => {
         const html = `<div class="card border-dark mb-3" id="dayOne" style="max-width: 18rem;">
-        <h4 id="date">${day.dt_txt.substring(0,9)}</h4>
+        <h4 id="date">${day.dt_txt.substring(0, 9)}</h4>
         <img class = "dayIcon" src="http://openweathermap.org/img/wn/${day.weather[0].icon}.png">
         <p>Temp: ${kelvToFar(day.main.temp).toFixed(0)}</p>
         <p>Humidity: ${day.main.humidity}</p>
     </div>`
-    $("#fiveRow").append(html);
+        $("#fiveRow").append(html);
     })
-  }
+}
 
-function renderCities(){
+if (cities.length !== 0) {
+    console.log(cities[cities.length - 1]);
+    var cityName = cities[cities.length - 1];
+    mainTxt.style.display = "block";
+    citiesList.style.display = "block";
+    citiesSec.style.display = "block";
+    daily.style.display = "block";
+    getAll();
+}
+
+function renderCities() {
     $("#citiesList").empty();
     cities.forEach(city => {
-        const html = `<button type="button" class="btn cityBtn col-2" data-name=${city}>${city}</button>`;
+        const html = `<button type="button" display="block" class="btn backColor m-2 col-10 cityBtn" data-name=${city}>${city}</button>`;
         $("#citiesList").prepend(html);
-    })
+        $(".cityBtn").on("click", function (city) {
+            cityName = this.getAttribute("data-name");
+            console.log("I was pressed");
+            console.log(cityName);
+            console.log(cities);
+            mainTxt.style.display = "block";
+            citiesList.style.display = "block";
+            citiesSec.style.display = "block";
+            daily.style.display = "block";
+
+            var queryString = makeUrl(cityName);
+            getAll();
+        }
+        )
+    }
+    )
 }
 
-function kelvToFar(temp){
-    return(temp - 273.15) * 1.80+32;
+
+function kelvToFar(temp) {
+    return (temp - 273.15) * 1.80 + 32;
 }
 
-function renderMain(current){
+function renderMain(current) {
 
     $(mainTxt).empty()
 
@@ -86,13 +117,13 @@ function renderMain(current){
     var iconNum = current.weather[0].icon;
     icons = "http://openweathermap.org/img/wn/" + iconNum + ".png"
     var imageTag = $("<img>").attr("src", icons);
-    
-    
+
+
 
     var tempPar = $("<p>");
     currentTemp = kelvToFar(current.main.temp);
     currentTemp = currentTemp.toFixed(0)
-    $(tempPar).html("Current Temperature: " + currentTemp + "&deg;"+"F");
+    $(tempPar).html("Current Temperature: " + currentTemp + "&deg;" + "F");
 
     var humPar = $("<p>");
     currentHum = current.main.humidity;
@@ -111,25 +142,25 @@ function renderMain(current){
     return cityName;
 }
 
-function renderUV(UVVal){
-    const html = `<p class="UV">UV Index: <span id="UVNum">${UVVal}</span></p>`
+function renderUV(UVVal) {
+    $("#UVID").empty();
+    const html = `<p id="UVID" class="UV">UV Index: <span id="UVNum">${UVVal}</span></p>`
     $(mainBody).append(html);
     var UVhtml = $(".UV")
     console.log(UVVal);
     if (UVVal <= 2) {
         UVhtml.addClass("low");
-    } else if (UVVal > 2 && UVVal <= 5){
+    } else if (UVVal > 2 && UVVal <= 5) {
         UVhtml.addClass("moderate");
-    } else if (UVVal > 5 && UVVal <= 7){
+    } else if (UVVal > 5 && UVVal <= 7) {
         UVhtml.addClass("high");
     } else UVhtml.addClass("veryHigh")
-}    
+}
 
 
-searchBtn.on("click", function(){
-    cityName=passText.val();
-    console.log("I was pressed");
-    if(!cities.includes(cityName)){
+searchBtn.on("click", function () {
+    cityName = passText.val();
+    if (!cities.includes(cityName)) {
         cities.push(cityName);
     }
     console.log(cityName);
@@ -138,39 +169,34 @@ searchBtn.on("click", function(){
     citiesList.style.display = "block";
     citiesSec.style.display = "block";
     daily.style.display = "block";
-    
 
+    getAll()
+
+})
+
+function getAll() {
     var queryString = makeUrl(cityName);
-   
-
     $.ajax({
         url: queryString,
         method: "GET"
-    }).then(function(current){
+    }).then(function (current) {
         renderMain(current);
         renderCities();
+        localStorage.setItem("cities", JSON.stringify(cities));
         var UVqueryString = makeUVUrl(current.coord.lon, current.coord.lat);
         $.ajax({
             url: UVqueryString,
             method: "GET"
-        }).then(function(UVData){
+        }).then(function (UVData) {
             renderUV(UVData.value);
             $.ajax({
                 url: fiveDayUrl(),
                 method: "GET"
-            }).then(function(data){
+            }).then(function (data) {
 
-                const fiveDay = data.list.filter((day,i)=> (i+1)%8 === 0);
+                const fiveDay = data.list.filter((day, i) => (i + 1) % 8 === 0);
                 renderFiveDay(fiveDay);
             })
         })
     })
-})
-
-// $("#cityBtn").on("click", function(){
-//     console.log(this);
-//     var cityName = this.data-name
-//     renderMain(cityName);
-//     renderUV(cityName);
-//     renderFiveDay(cityName);
-// })
+}
